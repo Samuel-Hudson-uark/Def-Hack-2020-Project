306fc78e794453ade839fdcb2b99cdceee0eee5a
SamuelHudson-UARK <59906089+SamuelHudson-UARK@users.noreply.github.com>
2020-06-20 16:18:49 -0500

    Basic movement and shooting

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody playerRB;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private Vector2 upperBounds;
+    private Vector2 lowerBounds;
+
+    public Shooter gun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        upperBounds = new Vector2(8f, 5f);
+        lowerBounds = new Vector2(-8f, -4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Basic function to read user input
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
+        moveVelocity = moveInput * moveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gun.isFiring = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            gun.isFiring = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 playerPos = playerRB.position;
+        playerRB.velocity = moveVelocity;
+        if(playerPos.x <= lowerBounds.x)
+            playerRB.position = new Vector3(lowerBounds.x, playerPos.y, playerPos.z);
+        if (playerPos.x >= upperBounds.x)
+            playerRB.position = new Vector3(upperBounds.x, playerPos.y, playerPos.z);
+        if (playerPos.y <= lowerBounds.y)
+            playerRB.position = new Vector3(playerPos.x, lowerBounds.y, playerPos.z);
+        if (playerPos.y >= upperBounds.y)
+            playerRB.position = new Vector3(playerPos.x, upperBounds.y, playerPos.z);
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public bool isFiring;
+
+    public Bullet bullet;
+    public float bulletSpeed;
+
+    public float timeBetweenShots, shotCounter, bulletLife;
+
+    public Transform firePoint1;
+    public Transform firePoint2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFiring)
+        {
+            shotCounter -= Time.deltaTime;
+            if (shotCounter <= 0)
+            {
+                shotCounter = timeBetweenShots;
+                Bullet newBullet = Instantiate(bullet, firePoint1.position, firePoint1.rotation) as Bullet;
+                newBullet.speed = bulletSpeed;
+                newBullet.life = bulletLife;
+                newBullet = Instantiate(bullet, firePoint2.position, firePoint2.rotation) as Bullet;
+                newBullet.speed = bulletSpeed;
+                newBullet.life = bulletLife;
+            }
+        }
+        else
+        {
+            shotCounter = 0;
+        }
+    }
+}
